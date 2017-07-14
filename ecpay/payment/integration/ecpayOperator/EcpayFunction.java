e79e2393ef808928e4a354464f4ce884a780d50d
allen.chang <allen.chang@allpay.com.tw>
2017-07-14 09:18:49 +0800

    修正Post傳入的字串格式(unicode string format to byte format)

--- ecpay/payment/integration/ecpayOperator/EcpayFunction.java
+++ ecpay/payment/integration/ecpayOperator/EcpayFunction.java
@@ -221,7 +221,7 @@ public class EcpayFunction {
 			connection.setRequestProperty("Accept-Language", encoding);
 			connection.setDoOutput(true);
 			DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-			wr.writeBytes(urlParameters);
+			wr.write(urlParameters.getBytes(encoding));
 			wr.flush();
 			wr.close();
 			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), encoding));
